5a62dc44098980f69803ed58ff16f2cdb792d64d
bryan <bdlepla@gmail.com>
2025-12-02 10:33:57 -0500

    Solved AOC2025 Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,109 @@
+import kotlin.collections.map
+
+enum class LockTurn {
+    Right,
+    Left
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    data class Instruction(val turn:LockTurn, val amount: Int )
+
+    // *******************
+    // Parsing
+    // *******************
+    fun String.toLockTurn():LockTurn = if (this == "L") LockTurn.Left else LockTurn.Right
+    fun List<String>.parse():List<Instruction> {
+        val regExp = Regex("([LR])(\\d+)")
+        return this
+            .map { regExp.matchEntire(it)!!.groups }
+            .map { Instruction(it[1]!!.value.toLockTurn(), it[2]!!.value.toInt())}
+    }
+
+
+
+    // *******************
+    // Part 1
+    // *******************
+    fun Instruction.turn1(position:Int):Int =
+        if (turn == LockTurn.Left) (100 + (position - amount)) % 100
+        else (position + amount) % 100
+
+
+    fun part1(input: List<String>):Int =
+        input.parse().runningFold(50) { acc, inst ->
+            inst.turn1(acc)
+        }.count{ it == 0}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // *******************
+    // Part 2
+    // *******************
+
+    // return Pair is newPosition, count of crossing/resting at Zero
+    fun Instruction.turn2(position:Int):Pair<Int, Int>  {
+        if (amount == 0) return position to 0
+
+        val fullRotations = amount / 100
+        val amountWithinOneRotation = amount % 100
+
+        return if (turn == LockTurn.Left) {
+            val tempNewPos = position - amountWithinOneRotation
+            val crossed = if (position != 0 && tempNewPos <= 0) 1 else 0
+            val newPos = (100 + tempNewPos) % 100
+            //println("$position $turn $amount => $tempNewPos $newPos $crossed $fullRotations")
+            newPos to fullRotations + crossed
+        }
+        else {
+            val tempNewPos = position + amountWithinOneRotation
+            val crossed = if (position != 0 && tempNewPos >= 100) 1 else 0
+            val newPos = tempNewPos % 100
+            //println("$position $turn $amount => $tempNewPos $newPos $crossed $fullRotations")
+            newPos to fullRotations + crossed
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.parse().fold(50 to 0) { acc, inst ->
+            val (position, zeroCount) = acc
+            val (newPosition, instrZeroCount) = inst.turn2(position)
+            newPosition to zeroCount + instrZeroCount
+        }.second
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // *******************
+    // Test code
+    // *******************
+    val testInput = """
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+    """.trimIndent().lines()
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
--- src/Utils.kt
+++ src/Utils.kt
@@ -3,6 +3,21 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
+typealias Coord = Pair<Int, Int>  // Row (Y), Col (X)
+typealias Grid = List<String>
+
+
+enum class Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest
+}
+
 /**
  * Reads lines from the given input txt file.
  */
@@ -11,11 +26,73 @@ fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+//fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+//    .toString(16)
+//    .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+/**
+ * Returns true if all elements in the collection match.
+ */
+fun <T>Iterable<T>.unanimous(): Boolean = this.all{it == this.first()}
+fun <T>Iterable<T>.allUnique():Boolean = this.count() == this.toSet().count()
+
+fun <T>List<T>.allTuples():List<Pair<T, T>> =
+    this.dropLast(1).flatMapIndexed{ idx, e ->
+        this.drop(idx+1).map { e2 -> e to e2 }
+    }
+
+fun Grid.get(coord:Coord) = this[coord.first][coord.second]
+fun Grid.isValid(coord:Coord) = coord.first in 0..<this.count() &&
+        coord.second in 0..<this[0].count()
+
+internal data class PQState<T>(val data:T, val step:Int)
+
+// after adding data with a cost,
+// the pop will return the data with the least cost
+class PriorityQueue<T> {
+    private val data = mutableListOf<PQState<T>>()
+    fun add(element:T, cost:Int) = add(PQState(element, cost))
+    private fun add(element:PQState<T>) {
+        val idx = data.binarySearch { element.step - it.step }
+        val insertIdx = if (idx < 0) (idx+1)*-1 else idx
+        data.add(insertIdx, element)
+    }
+    fun pop():Pair<T, Int>{
+        val ret = data.removeLast()
+        return ret.data to ret.step
+    }
+
+    fun isEmpty() = data.isEmpty()
+    fun isNotEmpty() = !isEmpty()
+}
+
+internal data class PQStateLong<T>(val data:T, val cost:Long)
+class PriorityQueueLong<T> {
+    private val data = mutableListOf<PQStateLong<T>>()
+    fun add(element:T, cost:Long) = add(PQStateLong(element, cost))
+    private fun add(element:PQStateLong<T>) {
+        val idx = data.binarySearch {
+            when {
+                element.cost < it.cost -> -1
+                element.cost > it.cost -> 1
+                else -> 0
+            }
+        }
+        val insertIdx = if (idx < 0) (idx+1)*-1 else idx
+        data.add(insertIdx, element)
+    }
+    fun pop():Pair<T, Long>{
+        val ret = data.removeLast()
+        return ret.data to ret.cost
+    }
+
+    fun isEmpty() = data.isEmpty()
+    fun isNotEmpty() = !isEmpty()
+}
+
+fun Int.raisedToPower(num:Int):Int = (0..<num).fold(1){a, _ -> a * this}
