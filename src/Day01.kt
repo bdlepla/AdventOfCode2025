a160ddcec57b4155f3e77089ac4d8408c5e174aa
bryan <bdlepla@gmail.com>
2025-12-04 15:18:28 -0500

    AOC2025 Day 02 - Refactor - use more idiomatic Kotlin. Use mod to force new position back to 0..99. Use value in enum class to remove the separate handling of turning left vs right.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,8 +1,8 @@
 import kotlin.collections.map
 
-enum class LockTurn {
-    Right,
-    Left
+enum class LockTurn(val mod:Int) {
+    Right(1),
+    Left(-1)
 }
 
 fun main() {
@@ -25,15 +25,10 @@ fun main() {
     // *******************
     // Part 1
     // *******************
-    fun Instruction.turn1(position:Int):Int =
-        if (turn == LockTurn.Left) (100 + (position - amount)) % 100
-        else (position + amount) % 100
-
+    fun Instruction.turn1(position:Int):Int = (position + (turn.mod * amount)).mod(100)
 
     fun part1(input: List<String>):Int =
-        input.parse().runningFold(50) { acc, inst ->
-            inst.turn1(acc)
-        }.count{ it == 0}
+        input.parse().runningFold(50) { acc, inst -> inst.turn1(acc) }.count{ it == 0 }
 
 
 
@@ -62,30 +57,19 @@ fun main() {
 
         val fullRotations = amount / 100
         val amountWithinOneRotation = amount % 100
-
-        return if (turn == LockTurn.Left) {
-            val tempNewPos = position - amountWithinOneRotation
-            val crossed = if (position != 0 && tempNewPos <= 0) 1 else 0
-            val newPos = (100 + tempNewPos) % 100
-            //println("$position $turn $amount => $tempNewPos $newPos $crossed $fullRotations")
-            newPos to fullRotations + crossed
-        }
-        else {
-            val tempNewPos = position + amountWithinOneRotation
-            val crossed = if (position != 0 && tempNewPos >= 100) 1 else 0
-            val newPos = tempNewPos % 100
-            //println("$position $turn $amount => $tempNewPos $newPos $crossed $fullRotations")
-            newPos to fullRotations + crossed
-        }
+        val tempNewPos = position + (turn.mod * amountWithinOneRotation)
+        val crossed = if (position != 0 && (tempNewPos !in 1..<100)) 1 else 0
+        val newPos = tempNewPos.mod(100)
+        //println("$position $turn $amount => $tempNewPos $newPos $crossed $fullRotations")
+        return newPos to fullRotations + crossed
     }
 
-    fun part2(input: List<String>): Int {
-        return input.parse().fold(50 to 0) { acc, inst ->
-            val (position, zeroCount) = acc
+    fun part2(input: List<String>): Int =
+        input.parse().fold(50 to 0) { (position, zeroCount), inst ->
             val (newPosition, instrZeroCount) = inst.turn2(position)
             newPosition to zeroCount + instrZeroCount
         }.second
-    }
+
 
     // *******************
     // Test code
@@ -103,7 +87,9 @@ fun main() {
         L82
     """.trimIndent().lines()
     check(part1(testInput) == 3)
+    println("Check Part 1 passed")
     check(part2(testInput) == 6)
+    println("Check Part 2 passed")
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
